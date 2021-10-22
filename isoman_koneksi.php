16f51a099880b4e801c8cd19f1e15f01e6ff9a2c
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-10-22 10:31:32 +0800

    Update isoman_koneksi.php

--- isoman_koneksi.php
+++ isoman_koneksi.php
@@ -1,5 +1,8 @@
 <?php
     $conn = mysqli_connect('us-cdbr-east-04.cleardb.com','b74b327ca0d25c','84c96737','heroku_9ad0d45b5e31016');
+    if(mysqli_connect_errno()){
+        echo "Fail";
+        }
     if(!$conn){
         echo 'Gagal terhubung';
     }
